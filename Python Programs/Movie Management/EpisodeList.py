e5656fb93e2d81a0d5562001d42c5520fb6ab4fe
Exonymos <83488939+Exonymos@users.noreply.github.com>
2023-03-15 13:11:09 +0530

    Update EpisodeList.py

--- Python Programs/Movie Management/EpisodeList.py
+++ Python Programs/Movie Management/EpisodeList.py
@@ -62,34 +62,98 @@ def add_entry():
 
 def update_watched_episodes():
     data = read_file()
-    print("Select the Movie/TV Show to update watched episodes:")
-    for i in range(len(data)):
-        print(f"{i+1}. {data[i]['name']}")
+    total_items = len(data)
+    page_size = 10
+    current_page = 1
     while True:
-        choice = input("Enter your choice (1-{}): ".format(len(data)))
-        if not choice.isdigit() or int(choice) < 1 or int(choice) > len(data):
-            print("Invalid input! Please enter a valid choice.")
-        else:
+        start_index = (current_page - 1) * page_size
+        end_index = start_index + page_size
+        items_to_display = data[start_index:end_index]
+        print("\nSelect the Movie/TV Show to update watched episodes:\n")
+        print("----------------------------")
+        for i, item in enumerate(items_to_display, start=start_index + 1):
+            print(f"{i}. {item['name']} ({item['watched_episodes']}/{item['total_episodes']})")
+        print("----------------------------")
+        print(f"Page {current_page} of {int((total_items - 1) / page_size) + 1}")
+        print("1. Next")
+        print("2. Previous")
+        print("3. Continue")
+        print("4. Back to menu")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            if end_index < total_items:
+                current_page += 1
+            else:
+                print("This is the last page.")
+        elif choice == "2":
+            if start_index > 0:
+                current_page -= 1
+            else:
+                print("This is the first page.")
+        elif choice == "3":
+            item_choice = int(input("Enter the number of the item to update: ")) - 1
+            if item_choice >= 0 and item_choice < len(items_to_display):
+                entry = items_to_display[item_choice]
+                entry_index = start_index + item_choice # calculate the index of the chosen item in the original data list
+                print(f'Updating {entry["name"]} ({entry["watched_episodes"]}/{entry["total_episodes"]})')
+                while True:
+                    watched_episodes = input("Enter number of episodes watched (or type 'back' to return to the menu): ")
+                    if watched_episodes == 'back':
+                        break
+                    elif not watched_episodes.isdigit():
+                        print("Invalid input! Please enter a numeric value.")
+                    elif int(watched_episodes) > entry["total_episodes"]:
+                        print("Invalid input! Watched episodes cannot be greater than total episodes.")
+                    else:
+                        entry["watched_episodes"] = int(watched_episodes)
+                        data[entry_index] = entry # update the original data list with the new watched episodes value
+                        write_file(data)
+                        print("Watched episodes updated successfully!")
+                        print(f'Current status for {entry["name"]}: {entry["watched_episodes"]}/{entry["total_episodes"]}')
+            else:
+                print("Invalid choice. Please enter a valid choice.")
+        elif choice == "4":
             break
-    entry = data[int(choice)-1]
-    print(f"Updating {entry['name']}")
-    while True:
-        watched_episodes = input("Enter number of episodes watched: ")
-        if not watched_episodes.isdigit():
-            print("Invalid input! Please enter a numeric value.")
-        elif int(watched_episodes) > entry["total_episodes"]:
-            print("Invalid input! Watched episodes cannot be greater than total episodes.")
         else:
-            entry["watched_episodes"] = int(watched_episodes)
-            break
-    write_file(data)
-    print("Watched episodes updated successfully!")
+            print("Invalid choice. Please enter a valid choice.")
+
+
+
 
 def rename_entry():
     data = read_file()
-    print("Select the Movie/TV Show to rename:")
-    for i in range(len(data)):
-        print(f"{i+1}. {data[i]['name']}")
+    total_items = len(data)
+    page_size = 10
+    current_page = 1
+    while True:
+        start_index = (current_page - 1) * page_size
+        end_index = start_index + page_size
+        items_to_display = data[start_index:end_index]
+        print("\nSelect the Movie/TV Show to rename:")
+        print("----------------------------")
+        for i, item in enumerate(items_to_display, start=start_index + 1):
+            print(f"{i}. {item['name']}")
+        print("----------------------------")
+        print(f"Page {current_page} of {int((total_items - 1) / page_size) + 1}")
+        print("1. Next")
+        print("2. Previous")
+        print("3. Back to menu")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            if end_index < total_items:
+                current_page += 1
+            else:
+                print("This is the last page.")
+        elif choice == "2":
+            if start_index > 0:
+                current_page -= 1
+            else:
+                print("This is the first page.")
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice. Please enter a valid choice.")
+
     while True:
         choice = input("Enter your choice (1-{}): ".format(len(data)))
         if not choice.isdigit() or int(choice) < 1 or int(choice) > len(data):
@@ -104,26 +168,53 @@ def rename_entry():
     
 def show_list():
     data = read_file()
-    print("\nList of Movies/TV Shows:")
-    print("-----------------------------")
-    for item in data:
-        print(item['name'])
-    print("-----------------------------\n")
+    total_items = len(data)
+    page_size = 10
+    current_page = 1
+    while True:
+        start_index = (current_page - 1) * page_size
+        end_index = start_index + page_size
+        items_to_display = data[start_index:end_index]
+        print("\nList of Movies/TV Shows:")
+        print("----------------------------")
+        for i, item in enumerate(items_to_display, start=start_index + 1):
+            print(f"{i}. {item['name']}")
+        print("----------------------------")
+        print(f"Page {current_page} of {int((total_items - 1) / page_size) + 1}")
+        print("1. Next")
+        print("2. Previous")
+        print("3. Back to menu")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            if end_index < total_items:
+                current_page += 1
+            else:
+                print("This is the last page.")
+        elif choice == "2":
+            if start_index > 0:
+                current_page -= 1
+            else:
+                print("This is the first page.")
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice. Please enter a valid choice.")
     
 def show_details():
     data = read_file()
     while True:
-        name = input("('!back' to return to the menu)\nEnter the name of the Movie/TV Show: ")
-        if name.lower() == '!back':
+        name_choice = input("('!back' to return to the menu)\nEnter the number of the Movie/TV Show name: ")
+        if name_choice.lower() == "!back":
             return
-        for item in data:
-            if name.lower() == item['name'].lower():
-                print(f"Name: {item['name']}")
-                print(f"Total episodes: {item['total_episodes']}")
-                print(f"Watched episodes: {item['watched_episodes']}")
-                print(f"Type: {item['type']}")
-                return
-        print("Entry not found!. Please enter the correct name.")
+        if not name_choice.isdigit() or int(name_choice) <= 0 or int(name_choice) > len(data):
+            print("Invalid choice! Please enter a valid number.")
+            continue
+        item = data[int(name_choice) - 1]
+        print(f"Name: {item['name']}")
+        print(f"Total episodes: {item['total_episodes']}")
+        print(f"Watched episodes: {item['watched_episodes']}")
+        print(f"Type: {item['type']}")
+        return
     
 def delete_entry():
     data = read_file()
@@ -177,6 +268,8 @@ def menu():
         elif choice == '5':
             delete_entry()
         elif choice == '6':
+            print("\nThank you for using this program!")
+            print("Created by Exonymos")
             print("Exiting program. Goodbye!")
             break
         else:
